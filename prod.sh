a1bf5e429bd73473ce0c821fb567a2f434a55c19
Sergey Vayser <wice24@gmail.com>
2018-01-02 19:06:16 +0200

    remove mongo data files

---
+++ prod.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+. ./utils.sh
+
+PROJECT=bot
+DOCKER_COMPOSE="docker-compose
+--project-name split-expense-${PROJECT}-dev
+-f docker-compose.prod.yml"
+
+case $1 in
+"up")
+  createNetwork
+
+  ${DOCKER_COMPOSE} pull --ignore-pull-failures
+  ${DOCKER_COMPOSE} build
+  ${DOCKER_COMPOSE} run --service-ports runner_bot bash
+  ${DOCKER_COMPOSE} down
+  ;;
+"down")
+  ${DOCKER_COMPOSE} down
+  ;;
+  *)
+  echo "first argument must be one of 'up' or 'down'"
+  exit 1;;
+esac
